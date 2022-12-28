d1ec1458df6a1125c52918d4992d4590e335e8e0
Wilson Lewis <wilsonlewis@gmail.com>
2022-12-28 12:43:45 -0600

    add env variable test

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { serve } from '@deno/http/server.ts';
 
 serve(() => {
+  const port = Deno.env.get('ENV_PORT');
+  console.log(`Port: ${port}`);
   return Response.json({ message: 'Hello World' });
 });
