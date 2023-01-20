e89d1c093492f9f55a55905e1e925805ec7c0b32
Wilson Lewis <wilsonlewis@gmail.com>
2023-01-20 04:26:38 -0600

    add websockets

--- main.ts
+++ main.ts
@@ -1,7 +1,34 @@
 import { serve } from '@deno/http/server.ts';
 
-serve(() => {
-  const port = Deno.env.get('ENV_PORT');
-  console.log(`Port: ${port}`);
-  return Response.json({ message: 'Hello World' });
+const port = Deno.env.get('ENV_PORT');
+
+serve((req) => {
+  const upgrade = req.headers.get('upgrade') || '';
+
+  if (upgrade.toLowerCase() != 'websocket') {
+    return new Response('request isn\'t trying to upgrade to websocket.');
+  }
+
+  const { socket, response } = Deno.upgradeWebSocket(req);
+
+  socket.onopen = () => {
+    console.log('socket opened');
+  };
+
+  socket.onmessage = (e) => {
+    console.log('socket message:', e.data);
+    socket.send(new Date().toString());
+  };
+
+  socket.onerror = (e) => {
+    console.log('socket errored:', e);
+  };
+
+  socket.onclose = () => {
+    console.log('socket closed');
+  };
+
+  return response;
+}, {
+  port: typeof port === 'string' ? Number(port) : undefined,
 });
